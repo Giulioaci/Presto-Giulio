a7503886ba38f1c8e876d6703f71f69f95bd8f75
Giulio Acitorio <giulioacitorio@yahoo.it>
2025-05-02 14:12:31 +0200

    section1 terminata

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+// Section2
+let FirstNumber = document.querySelector('#FirstNumber')
+let counter = 0
+
+let interval = setInterval( ()=>{
+   if(counter < 100){
+    counter++
+    FirstNumber.innerHTML = counter;
+   }else{
+        clearInterval(interval);
+   }
+
+},90);
 
