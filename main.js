48d25cafd9a1b7fda6a00fa457a6ef4175252bcc
Giulio Acitorio <giulioacitorio@yahoo.it>
2025-05-03 15:40:42 +0200

    section3 terminata

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 // Section2
 let FirstNumber = document.querySelector('#FirstNumber')
-let counter = 0
+let SecondNumber = document.querySelector('#SecondNumber')
+let ThirdNumber = document.querySelector('#ThirdNumber')
+let confirm = true;
+
+function CreateInterval(n, element, time) {
+   let counter = 0
 
 let interval = setInterval( ()=>{
-   if(counter < 100){
+   if(counter < n){
     counter++
-    FirstNumber.innerHTML = counter;
+    element.innerHTML = counter;
    }else{
         clearInterval(interval);
    }
 
-},90);
+},time);
+setTimeout(()=>{
+   confirm = true
+}, 9000);
+}
 
+let observer = new  IntersectionObserver( (entries)=> {
+     entries.forEach( (entry)=>{
+      if(entry.isIntersecting && confirm){
+         CreateInterval(100 , FirstNumber, 90);
+         CreateInterval(200, SecondNumber, 40);
+         CreateInterval(300, ThirdNumber, 25);
+         confirm = false
+      }
+     });
+});
+
+observer.observe(FirstNumber);
+
+// swiper
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'vertical',
+   loop: true,
+ 
+   autoplay: {
+      delay: 2000,
+    },
+ });
